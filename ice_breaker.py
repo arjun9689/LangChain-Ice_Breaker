3e99f72ce55106548bb6d8601e52e13421d85424
Nagarjun S <nagarjun9689@gmail.com>
2024-06-26 12:40:07 +0530

    scraping linkedin + mock

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,16 +5,16 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import LLMChains
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello LangChain!")
-
-    summary_template= """
-    given the information {information} about a person I want to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
+def ice_breaker_with(name:str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    summary_template = """
+        given the information {information} about a person I want to create:
+        1. A short summary
+        2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
@@ -25,7 +25,14 @@ if __name__ == "__main__":
 
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/nagarjun-s-b8470343/")
 
-    res = chain.invoke(input={"information":linkedin_data})
+    res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_breaker_with(name="Eden Marco")
 
